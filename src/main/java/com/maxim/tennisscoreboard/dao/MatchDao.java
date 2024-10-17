09f8911a83d50c98ca5304ff9deae9c5d944a33b
Maxim <karimovmaksim121@gmail.com>
2024-10-17 17:30:01 +0300

    add function public Match findByName(String name)

--- src/main/java/com/maxim/tennisscoreboard/dao/MatchDao.java
+++ src/main/java/com/maxim/tennisscoreboard/dao/MatchDao.java
@@ -1,9 +1,12 @@
 package com.maxim.tennisscoreboard.dao;
 
 import com.maxim.tennisscoreboard.models.Match;
+import com.maxim.tennisscoreboard.models.Player;
 import com.maxim.tennisscoreboard.util.HibernateSessionFactoryUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class MatchDao {
     public void save(Match match) {
@@ -11,6 +14,14 @@ public class MatchDao {
             Transaction tx1 = session.beginTransaction();
             session.persist(match);
             tx1.commit();
+        }
+    }
+
+    public List<Match> findByName(String name) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            String hql = "From Match where player1.name=:name or player2.name=:name";
+            List<Match> playerList = session.createQuery(hql).setParameter("name", name).getResultList();
+            return playerList;
         }
     }
 }
