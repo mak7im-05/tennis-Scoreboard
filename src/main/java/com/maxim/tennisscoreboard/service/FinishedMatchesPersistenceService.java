f247fc71a20834f2e1d0f1b3932c327736b6e737
Maxim <karimovmaksim121@gmail.com>
2024-10-24 11:01:55 +0300

    FIX Bag

--- src/main/java/com/maxim/tennisscoreboard/service/FinishedMatchesPersistenceService.java
+++ src/main/java/com/maxim/tennisscoreboard/service/FinishedMatchesPersistenceService.java
@@ -3,7 +3,6 @@ package com.maxim.tennisscoreboard.service;
 import com.maxim.tennisscoreboard.dao.MatchDao;
 import com.maxim.tennisscoreboard.models.Match;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FinishedMatchesPersistenceService {
@@ -15,23 +14,11 @@ public class FinishedMatchesPersistenceService {
         matchDao.save(match);
     }
 
-    public static List<Match> getAllMatches(int pageNumber) {
-        List<Match>matches = matchDao.getMatches();
-        return getMatchesPerPage(matches, pageNumber);
+    public static List<Match> getAllMatches() {
+        return matchDao.getMatches();
     }
 
-    public static List<Match> getMatchesByName(String filterByPlayerName, int pageNumber) {
-        List<Match> matches = matchDao.getMatchesByName(filterByPlayerName);
-        return getMatchesPerPage(matches, pageNumber);
-    }
-
-    private static List<Match> getMatchesPerPage(List<Match> allMatches, int pageNumber) {
-        List<Match> matches = new ArrayList<>();
-        if(!allMatches.isEmpty()) {
-            for (int i = (pageNumber-1)*MATCHES_PER_PAGE; i < pageNumber*MATCHES_PER_PAGE && i < allMatches.size(); i++) {
-                matches.add(allMatches.get(i));
-            }
-        }
-        return matches;
+    public static List<Match> getMatchesByName(String filterByPlayerName) {
+        return matchDao.getMatchesByName(filterByPlayerName);
     }
 }
--- src/main/java/com/maxim/tennisscoreboard/servlets/MatchesServlet.java
+++ src/main/java/com/maxim/tennisscoreboard/servlets/MatchesServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.maxim.tennisscoreboard.service.FinishedMatchesPersistenceService.MATCHES_PER_PAGE;
@@ -26,11 +27,13 @@ public class MatchesServlet extends HttpServlet {
             pageNumber = Integer.parseInt(request.getParameter("pageNumber"));
         }
 
-        matches = findMatchesByName(filterByPlayerName, pageNumber);
+        matches = findMatchesByName(filterByPlayerName);
 
         int totalPages = (int) (Math.ceil(matches.size() * 1.0 /MATCHES_PER_PAGE));
 
-        request.setAttribute("matches", matches);
+        List<Match> matchesPerPage = getMatchesPerPage(matches, pageNumber);
+
+        request.setAttribute("matches", matchesPerPage);
         request.setAttribute("size", matches.size());
         request.setAttribute("currentPage", pageNumber);
         request.setAttribute("totalPages", totalPages);
@@ -38,17 +41,27 @@ public class MatchesServlet extends HttpServlet {
         request.getRequestDispatcher("matches.jsp").forward(request,response);
      }
 
-    private List<Match> findMatchesByName(String filterByPlayerName, int pageNumber) {
+    private List<Match> findMatchesByName(String filterByPlayerName) {
         List<Match> matches;
         if(isFilterByPlayerNameNull(filterByPlayerName)) {
-            matches = FinishedMatchesPersistenceService.getMatchesByName(filterByPlayerName, pageNumber);
+            matches = FinishedMatchesPersistenceService.getMatchesByName(filterByPlayerName);
         } else {
-            matches = FinishedMatchesPersistenceService.getAllMatches(pageNumber);
+            matches = FinishedMatchesPersistenceService.getAllMatches();
         }
         return matches;
     }
 
     private boolean isFilterByPlayerNameNull(String filterByPlayerName) {
         return filterByPlayerName != null && !filterByPlayerName.isBlank();
+    }
+
+    private static List<Match> getMatchesPerPage(List<Match> allMatches, int pageNumber) {
+        List<Match> matches = new ArrayList<>();
+        if(!allMatches.isEmpty()) {
+            for (int i = (pageNumber-1)*MATCHES_PER_PAGE; i < pageNumber*MATCHES_PER_PAGE && i < allMatches.size(); i++) {
+                matches.add(allMatches.get(i));
+            }
+        }
+        return matches;
     }
 }
